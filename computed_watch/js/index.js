20ba008a26ec8d01f93ef1825ff496467d46546c
Tarcisio Neto <tarcisioneto1998@gmail.com>
2020-09-15 19:28:43 -0300

    Form adicionado, e um servidor express para testes.

--- computed_watch/js/index.js
+++ computed_watch/js/index.js
@@ -1,4 +1,24 @@
-var app = new Vue({
+/*var app = new Vue({
     el: "#app",
     data: {mensagem:'<p>Resident<br/> Evil</p>'}
+});*/
+
+var app = new Vue({
+    el:"#app",
+    data:{
+        nome:'',
+        email: ''
+    },
+
+    computed:{
+        getFullLogin: function(){
+            return this.nome + ' ' + this.email;
+        }
+    },
+
+    methods:{
+        connect: function(){
+            var obj = {login: this.getFullLogin()};
+        }
+    }
 });
---
+++ server/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var dados = [];
+var app = express();
+
+app.post('/', function(req, res){
+});
+
+app.get();
